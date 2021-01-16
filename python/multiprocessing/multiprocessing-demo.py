cdea2f5666a04ff2333c3329f4e86dee3531b87e
Joe Loesch <joe.wr.loesch@gmail.com>
2021-01-16 18:38:14 -0500

    Multiprocessing with for loops

--- python/multiprocessing/multiprocessing-demo.py
+++ python/multiprocessing/multiprocessing-demo.py
@@ -16,14 +16,15 @@ def do_something():
     print('Done sleeping...')
 
 def main():
-    p1 = multiprocessing.Process(target=do_something)
-    p2 = multiprocessing.Process(target=do_something)
+    processes = []
 
-    p1.start()
-    p2.start()
+    for _ in range(10):
+        p = multiprocessing.Process(target=do_something)
+        p.start()
+        processes.append(p)
 
-    p1.join()
-    p2.join()
+    for process in processes:
+        process.join()
 
     finish = time.perf_counter()
 
