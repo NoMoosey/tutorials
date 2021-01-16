2e024e8a547b5a4a84be06e4d59c44dd026ca9e5
Joe Loesch <joe.wr.loesch@gmail.com>
2021-01-16 18:56:37 -0500

    With ProcessPoolExecutor and list loop

--- python/multiprocessing/multiprocessing-demo.py
+++ python/multiprocessing/multiprocessing-demo.py
@@ -13,14 +13,15 @@ start = time.perf_counter()
 def do_something(seconds):
     print(f'Sleeping for {seconds} second(s)...')
     time.sleep(seconds)
-    return 'Done sleeping...'
+    return f'Done sleeping; slept {seconds}...'
 
 def main():
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        f1 = executor.submit(do_something, 1)
-        f2 = executor.submit(do_something, 1)
-        print(f1.result)
-        print(f2.result)
+        secs = [5, 4, 3, 2, 1]
+        results = [executor.submit(do_something, sec) for sec in secs]
+
+        for f in concurrent.futures.as_completed(results):
+            print(f.result())
 
     # OLD WAY:
     # processes = []
